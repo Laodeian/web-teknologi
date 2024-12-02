d59dd7d45ed2f7c77f50d90c6b46c5b6b2c7f510
la ode ian ian <laodeian21@gmail.com>
2024-12-02 16:32:51 +0700

    first commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,9 +10,13 @@ import {
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from '@/app/ui/button';
 import { useActionState } from 'react';
+import {authenticate} from 'app/lib/actions'; //autentikasi
 
 export default function Page() {
-  
+    const [errorMessage, formAction, isPending] = useActionState(
+        authenticate,
+        undefined,
+    );
     return (
         <main className="flex min-h-screen flex-col p-6">
             {/* Bagian Header */}
@@ -28,7 +32,7 @@ export default function Page() {
 
             {/* Bagian Form */}
             <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20 m-auto">
-                <form className="space-y-3">
+                <form action={formAction}className="space-y-3">
                     <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
                         {/* Judul */}
                         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
@@ -85,7 +89,11 @@ export default function Page() {
                             Masuk <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50"/>
                         </Button>
                         <div className="flex h-8 items-end space-x-1">
-                        
+                        {errorMessage && (
+                                <><ExclamationCircleIcon className="h-5 w5 text-red-500"/>
+                                <p className="text-sm text-red-500">{errorMessage}</p>
+                                </>
+                            )}
                         </div>
                     </div>
                 </form>
